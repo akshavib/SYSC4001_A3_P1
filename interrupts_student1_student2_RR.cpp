01537e96ba22f0c447a4c32e50f74f7f44663cdc
akshavibaskaran <akshavi.bt@gmail.com>
2025-11-30 00:06:51 -0500

    Taking code from EP and modifying it for RR, fore the base code

--- interrupts_student1_student2_RR.cpp
+++ interrupts_student1_student2_RR.cpp
@@ -89,6 +89,35 @@ std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std
             quantum = 0; // reset quantum timer
             execution_status += print_exec_status(current_time, running.PID, READY, RUNNING);
         }
+
+        if(running.PID != -1){ // handling if there is a running process
+            running.remaining_time--; // decrement remaining time of running process
+            running.time_until_next_io--; // decrement time until next IO interrupt
+            quantum++; // increment quantum timer 
+
+            sync_queue(job_list, running);
+        }
+
+        else if (running.io_freq > 0 && running.time_until_next_io <= 0){ // handling IO interrupt
+            running.state = WAITING;
+            running.remaining_io_time = running.io_duration; // set remaining IO time for waiting
+            running.time_until_next_io = running.io_freq; // reset time until next IO interrupt
+
+            wait_queue.push_back(running);
+            execution_status += print_exec_status(current_time + 1, running.PID, RUNNING, WAITING);
+
+            idle_CPU(running);
+            quantum = 0;
+        }
+
+        else if (quantum >= 100){ // handling quantum expiration
+            running.state = READY;
+            ready_queue.insert(ready_queue.begin(), running); // add running process to front of ready queue
+            execution_status += print_exec_status(current_time + 1, running.PID, RUNNING, READY);
+
+            idle_CPU(running);
+            quantum = 0;
+        }
         current_time++;
         /////////////////////////////////////////////////////////////////
 
