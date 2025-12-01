d506675e6d9ba502dd41292c6fecabd5863ccc3a
liamaddie <liamaddie@cmail.carleton.ca>
2025-11-30 23:31:47 -0500

    Ran all EP_RR tests

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ else
 fi
 
 #g++ -g -O0 -I . -o bin/interrupts_EP interrupts_student1_student2_EP.cpp 
-g++ -g -O0 -I . -o bin/interrupts_RR interrupts_student1_student2_RR.cpp
-#g++ -g -O0 -I . -o bin/interrupts_EP_RR interrupts_student1_student2_EP_RR.cpp
+#g++ -g -O0 -I . -o bin/interrupts_RR interrupts_student1_student2_RR.cpp
+g++ -g -O0 -I . -o bin/interrupts_EP_RR interrupts_student1_student2_EP_RR.cpp
 
 #./bin/interrupts_EP ./input_files/test1.txt 
-./bin/interrupts_RR ./input_files/test20.txt
-#./bin/interrupts_EP_RR ./input_files_test1.txt
+#./bin/interrupts_RR ./input_files/test1.txt
+./bin/interrupts_EP_RR ./input_files/test1.txt
